7eae7b81d359811b27a21433112c5c96ff223868
IsYet1 <Don@IsYet.com>
2016-08-06 17:32:16 -0500

    Added UWP. Builds.

---
+++ MyTunes.UWP/StreamLoader.cs
@@ -0,0 +1,24 @@
+﻿using MyTunes.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Windows.ApplicationModel;
+
+namespace MyTunes.UWP
+{
+    public class StreamLoader : IStreamLoader
+    {
+        public Stream GetStreamForFilename(string filename)
+        {
+            var storageFile = Package.Current.InstalledLocation.GetFileAsync(filename).AsTask().Result;
+
+            var stream = storageFile.OpenStreamForReadAsync().Result;
+
+            return stream;
+
+        }
+    }
+}
