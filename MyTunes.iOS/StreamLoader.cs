98c76d49cba6802001c2c4691d95fdf278b43e98
IsYet1 <Don@IsYet.com>
2016-08-06 17:10:35 -0500

    Added Ios.

---
+++ MyTunes.iOS/StreamLoader.cs
@@ -0,0 +1,16 @@
+﻿using MyTunes.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MyTunes
+{
+    public class StreamLoader : IStreamLoader
+    {
+        public Stream GetStreamForFilename(string filename)
+        {
+            return File.OpenRead(filename);
+        }
+    }
+}
