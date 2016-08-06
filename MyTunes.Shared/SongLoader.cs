2286ae86a31ddc92d717b46705b86eb7622d4f18
IsYet1 <Don@IsYet.com>
2016-08-06 17:33:49 -0500

    Remove conditional compile code from SongLoader

--- MyTunes.Shared/SongLoader.cs
+++ MyTunes.Shared/SongLoader.cs
@@ -10,7 +10,6 @@ namespace MyTunes
 	public static class SongLoader
 	{
 		const string Filename = "songs.json";
-#if ! WINDOWS_UWP
 
         public static async Task<IEnumerable<Song>> Load()
         {
@@ -22,41 +21,10 @@ namespace MyTunes
 
         private static Stream OpenData()
         {
-#if __IOS__
-            return File.OpenRead(Filename);
 
-#elif __ANDROID__
-            return Android.App.Application.Context.Assets.Open(Filename);
-
-#else
-            // TODO: add code to open file here.
-            return null;
-
-#endif
 
         }
 
-//It is UWP. Use Async methods instead.
-#else
-        public static async Task<IEnumerable<Song>> Load()
-        {
-            using (var reader = new StreamReader(await OpenData()))
-            {
-                return JsonConvert.DeserializeObject<List<Song>>(await reader.ReadToEndAsync());
-            }
-        }
-
-        private async static Task<Stream> OpenData()
-        {
-            var sf = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(Filename);
-            return await sf.OpenStreamForReadAsync();
-
-        // TODO: add code to open file here.
-            return null;
-
-        }
-
-#endif
     }
 }
 
